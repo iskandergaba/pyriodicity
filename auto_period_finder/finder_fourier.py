ffb4761ece64491899fe9d51f449f7aa9bb7e5ef
Iskander Gaba <iskander@hey.com>
2024-05-22 03:52:57 +0200

    Updated FourierPeriodFinder Documentation

--- auto_period_finder/finder_fourier.py
+++ auto_period_finder/finder_fourier.py
@@ -2,43 +2,33 @@ from typing import Optional, Union
 
 import numpy as np
 import pandas as pd
-from scipy.signal import get_window
 from statsmodels.tools.typing import ArrayLike1D
+from scipy.signal import get_window
 
 
 class FourierPeriodFinder:
     """
-    Autocorrelation function (ACF) based seasonality periods automatic finder.
+    Fast Fourier Transformation (FFT) based seasonality periods automatic finder.
 
-    Find the periods of a given time series using its ACF. A time delta
-    is considered a period if:
-        1- It is a local maximum of ACF.\n
-        2- Its multiples are also local maxima.\n
-        3- It is not a multiple of an already discovered period. For example,
-        it is redundant to return a 2 year seasonality period if we have already
-        found a 1 year period. The inverse, however, is not necessarily true.
+    Find the periods of a given time series using FFT.
 
     Parameters
     ----------
     endog : array_like
         Data to be investigated. Must be squeezable to 1-d.
-    acf_kwargs: dict, optional
-        Arguments to pass to the ACF.
 
     See Also
     --------
-    statsmodels.tsa.stattools.acf
-        Autocorrelation function.
-    statsmodels.tsa.seasonal.STL
-        Season-Trend decomposition using LOESS.
-    statsmodels.tsa.seasonal.seasonal_decompose
-        Seasonal decomposition using moving averages.
+    np.fft
+        Discrete Fourier Transform.
+    scipy.signal.get_window
+        SciPy window function.
 
     References
     ----------
     .. [1] Hyndman, R.J., & Athanasopoulos, G. (2021)
     Forecasting: principles and practice, 3rd edition, OTexts: Melbourne, Australia.
-    OTexts.com/fpp3/stlfeatures.html. Accessed on 12-23-2023.
+    OTexts.com/fpp3/useful-predictors.html#fourier-series. Accessed on 05-22-2024.
 
     Examples
     --------
@@ -49,17 +39,16 @@ class FourierPeriodFinder:
 
     You can resample the data to whatever frequency you want.
 
-    >>> data = data.resample("M").mean().ffill()
+    >>> data = data.resample("ME").mean().ffill()
 
-    Use AutoPeriodFinder to find the list of seasonality periods based on ACF.
+    Use FourierPeriodFinder to find the list of seasonality periods using FFT, ordered in.
 
-    >>> period_finder = AutoPeriodFinder(data)
+    >>> period_finder = FourierPeriodFinder(data)
     >>> periods = period_finder.fit()
 
-    You can also find the most prominent period either ACF-wise or variance-wise.
+    You can optionally specify a window function to pre-process with.
 
-    >>> strongest_period_acf = period_finder.fit_find_strongest_acf()
-    >>> strongest_period_var = period_finder.fit_find_strongest_var()
+    >>> periods = period_finder.fit(window_func="blackman")
     """
 
     def __init__(self, endog: ArrayLike1D):
@@ -68,25 +57,20 @@ class FourierPeriodFinder:
     def fit(
         self,
         max_period_count: Optional[Union[int, None]] = None,
-        window_func: Optional[Union[str, float, tuple, None]] = None,
+        window_func: Optional[Union[float, str, tuple, None]] = None,
     ) -> list:
         """
         Find seasonality periods of the given time series automatically.
 
         Parameters
         ----------
-        vicinity_radius : int, optional, default = None
-            How many data points, before and after, a period candidate
-            value to consider for satisfying the periodicity conditions.
-            Essentially, the algorithm will verify that at least one point
-            in the vicinity (defined by this parameter) of every multiple
-            of the candidate value is a local maximum.
-            This helps mitigate the effects of the forward and backward
-            noise shifts of the period value. It is also effective
-            at reducing the number of detected period values that are
-            too tightly bunched together.
         max_period_count : int, optional, default = None
             Maximum number of periods to look for.
+        window_func : float, str, tuple optional, default = None
+            Window function to be applied to the time series. Check
+            'window' parameter documentation for scipy.signal.get_window
+            function for more information on the accepted formats of this
+            parameter.
 
         Returns
         -------
@@ -105,20 +89,25 @@ class FourierPeriodFinder:
         max_period_count: Optional[Union[int, None]],
         window_func: Optional[Union[str, float, tuple, None]],
     ) -> list:
+        # Apply window function on the series
         y_windowed = (
             y
             if window_func is None
             else self.__get_windowed_y(y, window_func=window_func)
         )
 
-        # TODO: Rename `mags` and add some explanation comments
-        ft = np.fft.rfft(y_windowed)[1:]
-        mags = abs(ft)
+        # Compute DFT and ignore the zero frequency
         freqs = np.fft.rfftfreq(len(y_windowed), d=1)[1:]
+        ft = np.fft.rfft(y_windowed)[1:]
+
+        # Compute periods and their respective amplitudes
         periods = np.round(1 / freqs)
+        amps = abs(ft)
 
         # A period cannot be greater than half the length of the series
-        result = pd.Series(index=periods, data=mags)[periods < len(y_windowed) / 2]
+        result = pd.Series(index=periods, data=amps)[periods < len(y_windowed) // 2]
+
+        # Return periods in descending order of amplitudes
         return (
             result.sort_values(ascending=False).index.unique().values[:max_period_count]
         )
