c25f4fc7ca8586b0fe6060fd7cdd709d8d3203a6
Iskander <iskander@hey.com>
2024-08-07 13:36:25 +0100

    Add new tool functions
    Add the functions:
    - 'acf': Calculate the autocorrelation function of a given data array.
    - 'apply_window': Apply window function on a given data array. To
    replace 'apply_window_fun'.
    - 'detrend': Detrend a given data array.

--- auto_period_finder/tools/__init__.py
+++ auto_period_finder/tools/__init__.py
@@ -1,12 +1,18 @@
 from ._tools import (
+    acf,
+    apply_window,
     apply_window_fun,
+    detrend,
     remove_overloaded_kwargs,
     seasonality_strength,
     to_1d_array,
 )
 
 __all__ = [
+    "acf",
+    "apply_window",
     "apply_window_fun",
+    "detrend",
     "remove_overloaded_kwargs",
     "seasonality_strength",
     "to_1d_array",
--- auto_period_finder/tools/_tools.py
+++ auto_period_finder/tools/_tools.py
@@ -1,9 +1,11 @@
-from typing import Dict, List, Union
+from typing import Callable, Dict, List, Optional, Union
 
 import numpy as np
 from numpy.typing import ArrayLike, NDArray
 from pandas import DataFrame, Series
+from scipy.signal import detrend as _detrend
 from scipy.signal import get_window
+from scipy.stats import kendalltau, pearsonr, spearmanr
 
 
 @staticmethod
@@ -21,6 +23,7 @@ def remove_overloaded_kwargs(kwargs: Dict, args: List) -> Dict:
     return kwargs
 
 
+# Deprecated
 @staticmethod
 def apply_window_fun(x: ArrayLike, window_func: Union[str, float, tuple]) -> NDArray:
     return (x - np.median(x)) * get_window(window=window_func, Nx=len(x))
@@ -29,3 +32,33 @@ def apply_window_fun(x: ArrayLike, window_func: Union[str, float, tuple]) -> NDA
 @staticmethod
 def seasonality_strength(seasonal: ArrayLike, resid: ArrayLike) -> float:
     return max(0, 1 - np.var(resid) / np.var(seasonal + resid))
+
+
+@staticmethod
+def apply_window(x: ArrayLike, window_func: Union[str, float, tuple]) -> NDArray:
+    return x * get_window(window=window_func, Nx=len(x))
+
+
+@staticmethod
+def detrend(
+    x: Union[ArrayLike, DataFrame, Series],
+    method: Union[str, Callable[[Union[ArrayLike, DataFrame, Series]], NDArray]],
+) -> NDArray:
+    if isinstance(method, str):
+        return _detrend(x, type=method)
+    return method(x)
+
+
+@staticmethod
+def acf(
+    x: Union[ArrayLike, DataFrame, Series],
+    nlags: int,
+    correlation_func: Optional[str] = "pearson",
+) -> NDArray:
+    if not 0 < nlags <= len(x):
+        raise ValueError("nlags must be a postive integer less than the data length")
+    if correlation_func == "spearman":
+        return np.array([spearmanr(x, np.roll(x, l)).statistic for l in range(nlags)])
+    elif correlation_func == "kendall":
+        return np.array([kendalltau(x, np.roll(x, l)).statistic for l in range(nlags)])
+    return np.array([pearsonr(x, np.roll(x, l)).statistic for l in range(nlags)])
