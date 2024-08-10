1766aa310f758dbce531eb5360b3446e31371327
Iskander <iskander@hey.com>
2024-08-10 14:35:41 +0100

    Improve ACF period finder
    Simplify ACF period finder by removing seasonal decomposition options,
    adding detrending and window application options, adding other possible
    correlation funcions, and adding more unit tests

--- auto_period_finder/finders/finder_acf.py
+++ auto_period_finder/finders/finder_acf.py
@@ -1,18 +1,9 @@
-from typing import Dict, Optional, Union
+from typing import Callable, Optional, Union
 
-import numpy as np
 from numpy.typing import ArrayLike, NDArray
-from pandas import DataFrame, Series
 from scipy.signal import argrelmax
-from statsmodels.tsa.seasonal import STL, seasonal_decompose
-from statsmodels.tsa.stattools import acf
 
-from auto_period_finder.enums import TimeSeriesDecomposer
-from auto_period_finder.tools import (
-    remove_overloaded_kwargs,
-    seasonality_strength,
-    to_1d_array,
-)
+from auto_period_finder.tools import acf, apply_window, detrend, to_1d_array
 
 
 class AutocorrelationPeriodFinder:
@@ -20,23 +11,12 @@ class AutocorrelationPeriodFinder:
     Autocorrelation function (ACF) based seasonality periods automatic finder.
 
     Find the periods of a given time series using its ACF. A time delta
-    is considered a period if it is a local maximum of ACF.\n
+    is considered a period if it is a local maximum of ACF.
 
     Parameters
     ----------
     endog : array_like
         Data to be investigated. Must be squeezable to 1-d.
-    acf_kwargs: dict, optional
-        Arguments to pass to the ACF.
-
-    See Also
-    --------
-    statsmodels.tsa.stattools.acf
-        Autocorrelation function.
-    statsmodels.tsa.seasonal.STL
-        Season-Trend decomposition using LOESS.
-    statsmodels.tsa.seasonal.seasonal_decompose
-        Seasonal decomposition using moving averages.
 
     References
     ----------
@@ -61,25 +41,24 @@ class AutocorrelationPeriodFinder:
     >>> period_finder = AutocorrelationPeriodFinder(data)
     >>> periods = period_finder.fit()
 
-    You can also find the most prominent period either ACF-wise or variance-wise.
+    You can get the most prominent period by setting max_period_count to 1
 
-    >>> strongest_period_acf = period_finder.fit_find_strongest_acf()
-    >>> strongest_period_var = period_finder.fit_find_strongest_var()
+    >>> period_finder.fit(max_period_count=1)
+
+    You can also use a different correlation function like Spearman
+
+    >>> period_finder.fit(correlation_func="spearman")
     """
 
-    def __init__(
-        self,
-        endog: Union[ArrayLike, DataFrame, Series],
-        acf_kwargs: Optional[Dict[str, Union[int, bool, None]]] = None,
-    ):
+    def __init__(self, endog: ArrayLike):
         self.y = to_1d_array(endog)
-        self._acf_kwargs = self.__remove_overloaded_acf_kwargs(
-            acf_kwargs if acf_kwargs else {}
-        )
 
     def fit(
         self,
-        max_period_count: Optional[Union[int, None]] = None,
+        max_period_count: Optional[int] = None,
+        detrend_func: Optional[Union[str, Callable[[ArrayLike], NDArray]]] = "linear",
+        window_func: Optional[Union[str, float, tuple]] = None,
+        correlation_func: Optional[str] = "pearson",
     ) -> NDArray:
         """
         Find seasonality periods of the given time series automatically.
@@ -88,117 +67,65 @@ class AutocorrelationPeriodFinder:
         ----------
         max_period_count : int, optional, default = None
             Maximum number of periods to look for.
+        detrend_func : str, callable, default = None
+            The kind of detrending to be applied on the series. It can either be
+            'linear' or 'constant' if it the parameter is of 'str' type, or a
+            custom function that returns a detrended series.
+        window_func : float, str, tuple optional, default = None
+            Window function to be applied to the time series. Check
+            'window' parameter documentation for scipy.signal.get_window
+            function for more information on the accepted formats of this
+            parameter.
+        correlation_func : str, default = 'pearson'
+            The correlation function to be used to calculate the ACF of the time
+            series. Possible values are ['pearson', 'spearman', 'kendall'].
+
+        See Also
+        --------
+        scipy.signal.detrend
+            Remove linear trend along axis from data.
+        scipy.signal.get_window
+            Return a window of a given length and type.
+        scipy.stats.kendalltau
+            Calculate Kendall's tau, a correlation measure for ordinal data.
+        scipy.stats.pearsonr
+            Pearson correlation coefficient and p-value for testing non-correlation.
+        scipy.stats.spearmanr
+            Calculate a Spearman correlation coefficient with associated p-value.
+
 
         Returns
         -------
         NDArray
-            List of periods.
+            List of detected seasonality periods.
         """
-        return self.__find_periods(self.y, max_period_count, self._acf_kwargs)
-
-    def fit_find_strongest_acf(self) -> np.int64:
-        """
-        Find the strongest seasonality period ACF-wise of the given time series.
-
-        Returns
-        -------
-        np.int64
-            The strongest seasonality period ACF-wise.
-        """
-        periods = self.fit(max_period_count=1)
-        if len(periods) == 0:
-            return None
-        else:
-            return periods[0]
-
-    def fit_find_strongest_var(
-        self,
-        max_period_count: Optional[Union[int, None]] = None,
-        decomposer: Optional[
-            TimeSeriesDecomposer
-        ] = TimeSeriesDecomposer.MOVING_AVERAGES,
-        decomposer_kwargs: Optional[Dict[str, Union[int, bool, None]]] = None,
-    ) -> np.int64:
-        """
-        Find the strongest seasonality period variance-wise of the given time series
-        using seasonal decomposition.
-
-        Parameters
-        ----------
-        max_period_count : int, optional, default = None
-            Maximum number of periods to look for.
-        decomposer: TimeSeriesDecomposer. optional,
-        default = TimeSeriesDecomposer.MOVING_AVERAGE_DECOMPOSER
-            The seasonality decomposer that returns DecomposeResult to be used to
-            determine the strongest seasonality period. The possible values are
-            [TimeSeriesDecomposer.MOVING_AVERAGE_DECOMPOSER, TimeSeriesDecomposer.STL].
-        decomposer_kwargs: dict, optional
-            Arguments to pass to the decomposer.
-
-        Returns
-        -------
-        np.int64
-            The strongest seasonality period.
-        """
-        periods = self.fit(max_period_count=max_period_count)
-        if len(periods) == 0:
-            return None
-        elif len(periods) == 1:
-            return periods[0]
-        else:
-            if decomposer == TimeSeriesDecomposer.STL:
-                decomposer_kwargs = self.__remove_overloaded_stl_kwargs(
-                    decomposer_kwargs if decomposer_kwargs else {}
-                )
-                decomps = {
-                    p: STL(self.y, p, **decomposer_kwargs).fit() for p in periods
-                }
-            elif decomposer == TimeSeriesDecomposer.MOVING_AVERAGES:
-                decomposer_kwargs = self.__remove_overloaded_seasonal_decompose_kwargs(
-                    decomposer_kwargs if decomposer_kwargs else {}
-                )
-                decomps = {
-                    p: seasonal_decompose(self.y, period=p, **decomposer_kwargs)
-                    for p in periods
-                }
-            else:
-                raise ValueError("Invalid seasonality decomposer: " + decomposer)
-        strengths = {
-            p: seasonality_strength(d.seasonal, d.resid) for p, d in decomps.items()
-        }
-        return max(strengths, key=strengths.get)
+        return self.__find_periods(
+            self.y, max_period_count, detrend_func, window_func, correlation_func
+        )
 
     def __find_periods(
         self,
-        y: Union[ArrayLike, DataFrame, Series],
-        max_period_count: Optional[Union[int, None]],
-        acf_kwargs: Dict[str, Union[int, bool, None]],
+        y: ArrayLike,
+        max_period_count: Optional[int],
+        detrend_func: Optional[Union[str, Callable[[ArrayLike], NDArray]]] = "linear",
+        window_func: Optional[Union[str, float, tuple]] = None,
+        correlation_func: Optional[str] = "pearson",
     ) -> NDArray:
-        # Calculate the autocorrelation function
-        acf_arr = acf(y, nlags=len(y) // 2, **acf_kwargs)
 
-        # Find local maxima of the first half of the ACF array
+        # Detrend data
+        self.y = self.y if detrend_func is None else detrend(self.y, detrend_func)
+
+        # Apply window on data
+        self.y = self.y if window_func is None else apply_window(self.y, window_func)
+
+        # Compute the ACF
+        acf_arr = acf(self.y, len(y) // 2, correlation_func)
+
+        # Find the local argmax of the first half of the ACF array
         local_argmax = argrelmax(acf_arr)[0]
 
-        # Arg. sort the local maxima in the ACF array in a descending order
+        # Argsort the local maxima in the ACF array in a descending order
         periods = local_argmax[acf_arr[local_argmax].argsort()][::-1]
 
-        # Return the requested maximum count of detectde periods
+        # Return the requested maximum count of detected periods
         return periods[:max_period_count]
-
-    @staticmethod
-    def __remove_overloaded_acf_kwargs(acf_kwargs: Dict) -> Dict:
-        args = ["x", "nlags", "qstat", "alpha", "bartlett_confint"]
-        return remove_overloaded_kwargs(acf_kwargs, args)
-
-    @staticmethod
-    def __remove_overloaded_stl_kwargs(stl_kwargs: Dict) -> Dict:
-        args = ["endog", "period"]
-        return remove_overloaded_kwargs(stl_kwargs, args)
-
-    @staticmethod
-    def __remove_overloaded_seasonal_decompose_kwargs(
-        seasonal_decompose_kwargs: Dict,
-    ) -> Dict:
-        args = ["x", "period"]
-        return remove_overloaded_kwargs(seasonal_decompose_kwargs, args)
--- tests/finders/test_finder_acf.py
+++ tests/finders/test_finder_acf.py
@@ -1,59 +1,96 @@
 from statsmodels.datasets import co2
 
 from auto_period_finder import AutocorrelationPeriodFinder
-from auto_period_finder.enums import TimeSeriesDecomposer
 
 
-def test_co2_monthly_acf_find_all_periods():
+def test_co2_daily_acf_default():
+    data = co2.load().data.resample("D").mean().ffill()
+    period_finder = AutocorrelationPeriodFinder(data)
+    periods = period_finder.fit()
+    assert len(periods) > 0
+
+
+def test_co2_weekly_acf_default():
+    data = co2.load().data.resample("W").mean().ffill()
+    period_finder = AutocorrelationPeriodFinder(data)
+    periods = period_finder.fit()
+    assert len(periods) > 0
+
+
+def test_co2_monthly_acf_default():
     data = co2.load().data.resample("ME").mean().ffill()
     period_finder = AutocorrelationPeriodFinder(data)
     periods = period_finder.fit()
-    assert len(periods) != 0
+    assert len(periods) > 0
 
 
-def test_co2_monthly_acf_find_first_two_periods():
-    data = co2.load().data.resample("ME").mean().ffill()
+def test_co2_daily_acf_max_period_count_one():
+    data = co2.load().data.resample("D").mean().ffill()
     period_finder = AutocorrelationPeriodFinder(data)
-    periods = period_finder.fit(max_period_count=2)
-    assert len(periods) == 2
+    periods = period_finder.fit(max_period_count=1)
+    assert len(periods) == 1
+    assert periods[0] == 364
 
 
-def test_co2_monthly_acf_find_strongest_period_acf_wise():
+def test_co2_weekly_acf_max_period_count_one():
+    data = co2.load().data.resample("W").mean().ffill()
+    period_finder = AutocorrelationPeriodFinder(data)
+    periods = period_finder.fit(max_period_count=1)
+    assert len(periods) == 1
+    assert periods[0] == 52
+
+
+def test_co2_monthly_acf_max_period_count_one():
     data = co2.load().data.resample("ME").mean().ffill()
     period_finder = AutocorrelationPeriodFinder(data)
     periods = period_finder.fit(max_period_count=1)
-    strongest_period_acf = period_finder.fit_find_strongest_acf()
     assert len(periods) == 1
-    assert periods[0] == strongest_period_acf
+    assert periods[0] == 12
 
 
-def test_co2_monthly_acf_find_strongest_period_var_wise_stl_default():
-    data = co2.load().data.resample("ME").mean().ffill()
-    period_finder = AutocorrelationPeriodFinder(data)
-    strongest_period_var = period_finder.fit_find_strongest_var(
-        decomposer=TimeSeriesDecomposer.STL
-    )
-    assert strongest_period_var == 82
-
-
-def test_co2_monthly_acf_find_strongest_period_var_wise_stl_custom():
-    data = co2.load().data.resample("ME").mean().ffill()
-    period_finder = AutocorrelationPeriodFinder(data)
-    strongest_period_var = period_finder.fit_find_strongest_var(
-        decomposer=TimeSeriesDecomposer.STL, decomposer_kwargs={"seasonal_deg": 0}
-    )
-    assert strongest_period_var == 10
-
-
-def test_co2_daily_acf_find_strongest_period_var_wise_moving_averages():
+def test_co2_daily_acf_max_period_count_one_correlation_func_spearman():
     data = co2.load().data.resample("D").mean().ffill()
     period_finder = AutocorrelationPeriodFinder(data)
-    strongest_period_var = period_finder.fit_find_strongest_var()
-    assert strongest_period_var == 322
+    periods = period_finder.fit(max_period_count=1, correlation_func="spearman")
+    assert len(periods) == 1
+    assert periods[0] == 364
 
 
-def test_co2_monthly_acf_find_strongest_period_var_wise_moving_averages():
+def test_co2_weekly_acf_max_period_count_one_correlation_func_spearman():
+    data = co2.load().data.resample("W").mean().ffill()
+    period_finder = AutocorrelationPeriodFinder(data)
+    periods = period_finder.fit(max_period_count=1, correlation_func="spearman")
+    assert len(periods) == 1
+    assert periods[0] == 52
+
+
+def test_co2_monthly_acf_max_period_count_one_correlation_func_spearman():
     data = co2.load().data.resample("ME").mean().ffill()
     period_finder = AutocorrelationPeriodFinder(data)
-    strongest_period_var = period_finder.fit_find_strongest_var()
-    assert strongest_period_var == 10
+    periods = period_finder.fit(max_period_count=1, correlation_func="spearman")
+    assert len(periods) == 1
+    assert periods[0] == 12
+
+
+def test_co2_daily_acf_max_period_count_one_window_func_blackman():
+    data = co2.load().data.resample("D").mean().ffill()
+    period_finder = AutocorrelationPeriodFinder(data)
+    periods = period_finder.fit(max_period_count=1, window_func="blackman")
+    assert len(periods) == 1
+    assert periods[0] == 364
+
+
+def test_co2_weekly_acf_max_period_count_one_window_func_blackman():
+    data = co2.load().data.resample("W").mean().ffill()
+    period_finder = AutocorrelationPeriodFinder(data)
+    periods = period_finder.fit(max_period_count=1, window_func="blackman")
+    assert len(periods) == 1
+    assert periods[0] == 52
+
+
+def test_co2_monthly_acf_max_period_count_one_window_func_blackman():
+    data = co2.load().data.resample("ME").mean().ffill()
+    period_finder = AutocorrelationPeriodFinder(data)
+    periods = period_finder.fit(max_period_count=1, window_func="blackman")
+    assert len(periods) == 1
+    assert periods[0] == 12
