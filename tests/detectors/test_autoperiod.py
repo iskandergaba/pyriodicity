08ac1c4a8324a8d07626ea6500f84aef013e4e21
Iskander Gaba <iskander@hey.com>
2024-10-07 17:06:16 +0200

    Remove some tests for Autoperiod

--- tests/detectors/test_autoperiod.py
+++ tests/detectors/test_autoperiod.py
@@ -1,4 +1,3 @@
-import numpy as np
 from statsmodels.datasets import co2
 
 from pyriodicity import Autoperiod
@@ -69,35 +68,5 @@ def test_co2_monthly_autoperiod_detrend_func_constant_window_func_blackman():
     data = co2.load().data.resample("ME").mean().ffill()
     autoperiod = Autoperiod(data)
     periods = autoperiod.fit(detrend_func="constant", window_func="blackman")
-    assert len(periods) == 1
-    assert 12 in periods
-
-
-def test_co2_daily_autoperiod_detrend_func_custom_window_func_blackmanharris():
-    data = co2.load().data.resample("D").mean().ffill()
-    autoperiod = Autoperiod(data)
-    periods = autoperiod.fit(
-        detrend_func=lambda x: x - np.median(x), window_func="blackmanharris"
-    )
-    assert len(periods) == 1
-    assert 364 in periods
-
-
-def test_co2_weekly_autoperiod_detrend_func_custom_window_func_blackmanharris():
-    data = co2.load().data.resample("W").mean().ffill()
-    autoperiod = Autoperiod(data)
-    periods = autoperiod.fit(
-        detrend_func=lambda x: x - np.median(x), window_func="blackmanharris"
-    )
-    assert len(periods) == 1
-    assert 52 in periods
-
-
-def test_co2_monthly_autoperiod_detrend_func_custom_window_func_blackmanharris():
-    data = co2.load().data.resample("ME").mean().ffill()
-    autoperiod = Autoperiod(data)
-    periods = autoperiod.fit(
-        detrend_func=lambda x: x - np.median(x), window_func="blackmanharris"
-    )
     assert len(periods) == 1
     assert 12 in periods
