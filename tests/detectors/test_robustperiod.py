34ec9e1168a482f6a577a23f9804c1a6daed8356
Iskander Gaba <iskander@hey.com>
2024-12-28 01:57:36 +0100

    Increase test coverage a little bit

--- tests/detectors/test_robustperiod.py
+++ tests/detectors/test_robustperiod.py
@@ -13,3 +13,15 @@ def test_co2_monthly_robustperiod_default(co2_monthly):
     periods = RobustPeriod.detect(data)
     assert len(periods) > 0
     assert 12 in periods
+
+
+def test_co2_weekly_robustperiod_max_period_count_one(co2_weekly):
+    data = co2_weekly
+    periods = RobustPeriod.detect(data, max_period_count=1)
+    assert len(periods) == 1
+
+
+def test_co2_monthly_robustperiod_max_period_count_one(co2_monthly):
+    data = co2_monthly
+    periods = RobustPeriod.detect(data, max_period_count=1)
+    assert len(periods) == 1
