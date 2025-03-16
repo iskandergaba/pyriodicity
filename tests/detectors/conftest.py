3358d754387d635f7d8dae7a18cb9bf8eae3257c
Iskander Gaba <iskander@hey.com>
2025-03-16 16:19:21 +0100

    Implement test_online_fft.py

--- tests/detectors/conftest.py
+++ tests/detectors/conftest.py
@@ -1,3 +1,5 @@
+from typing import Generator
+
 import numpy as np
 import pytest
 from scipy.signal import sawtooth
@@ -19,6 +21,21 @@ def sinewave_100():
     return sinewave(1000, 100, 1)
 
 
+@pytest.fixture
+def sinewave_10_generator() -> Generator[float, None, None]:
+    return (sample for sample in sinewave(1000, 10, 1))
+
+
+@pytest.fixture
+def sinewave_50_generator() -> Generator[float, None, None]:
+    return (sample for sample in sinewave(1000, 50, 1))
+
+
+@pytest.fixture
+def sinewave_100_generator() -> Generator[float, None, None]:
+    return (sample for sample in sinewave(1000, 100, 1))
+
+
 @pytest.fixture(scope="module")
 def trianglewave_10():
     return trianglewave(1000, 10, 1)
@@ -34,6 +51,21 @@ def trianglewave_100():
     return trianglewave(1000, 100, 1)
 
 
+@pytest.fixture
+def trianglewave_10_generator() -> Generator[float, None, None]:
+    return (sample for sample in trianglewave(1000, 10, 1))
+
+
+@pytest.fixture
+def trianglewave_50_generator() -> Generator[float, None, None]:
+    return (sample for sample in trianglewave(1000, 50, 1))
+
+
+@pytest.fixture
+def trianglewave_100_generator() -> Generator[float, None, None]:
+    return (sample for sample in trianglewave(1000, 100, 1))
+
+
 @pytest.fixture(scope="module")
 def co2_weekly():
     return co2_data()
@@ -42,6 +74,16 @@ def co2_weekly():
 @pytest.fixture(scope="module")
 def co2_monthly():
     return co2_data().resample("ME").mean()
+
+
+@pytest.fixture
+def co2_weekly_generator():
+    return (sample for sample in co2_data().values)
+
+
+@pytest.fixture
+def co2_monthly_generator():
+    return (sample for sample in co2_data().resample("ME").mean().values)
 
 
 def sinewave(n, period, amp):
