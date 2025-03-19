2f6e58deba600ea3c9241412a3fafaea7628e532
Iskander Gaba <iskander@hey.com>
2025-03-19 14:47:05 +0100

    Tweak test_autoperiod.py

--- tests/static/test_autoperiod.py
+++ tests/static/test_autoperiod.py
@@ -1,39 +1,11 @@
 from pyriodicity import Autoperiod
 
 
-def test_sinewave_10_autoperiod_find_all_periods(sinewave_10):
-    data = sinewave_10
-    periods = Autoperiod.detect(data)
-    assert len(periods) > 0
-    assert 10 in periods
-
-
-def test_sinewave_50_autoperiod_find_all_periods(sinewave_50):
-    data = sinewave_50
-    periods = Autoperiod.detect(data)
-    assert len(periods) > 0
-    assert 50 in periods
-
-
 def test_sinewave_100_autoperiod_find_all_periods(sinewave_100):
     data = sinewave_100
     periods = Autoperiod.detect(data)
     assert len(periods) > 0
     assert 100 in periods
-
-
-def test_trianglewave_10_autoperiod_find_all_periods(trianglewave_10):
-    data = trianglewave_10
-    periods = Autoperiod.detect(data)
-    assert len(periods) > 0
-    assert 10 in periods
-
-
-def test_trianglewave_50_autoperiod_find_all_periods(trianglewave_50):
-    data = trianglewave_50
-    periods = Autoperiod.detect(data)
-    assert len(periods) > 0
-    assert 50 in periods
 
 
 def test_trianglewave_100_autoperiod_find_all_periods(trianglewave_100):
@@ -60,25 +32,25 @@ def test_co2_monthly_autoperiod_default(co2_monthly):
 def test_co2_weekly_autoperiod_detrend_func_none(co2_weekly):
     data = co2_weekly
     periods = Autoperiod.detect(data, detrend_func=None)
-    assert len(periods) == 0
+    assert 52 not in periods
 
 
 def test_co2_monthly_autoperiod_detrend_func_none(co2_monthly):
     data = co2_monthly
     periods = Autoperiod.detect(data, detrend_func=None)
-    assert len(periods) == 0
+    assert 12 not in periods
 
 
 def test_co2_weekly_autoperiod_detrend_func_constant(co2_weekly):
     data = co2_weekly
     periods = Autoperiod.detect(data, detrend_func="constant")
-    assert len(periods) == 0
+    assert 52 not in periods
 
 
 def test_co2_monthly_autoperiod_detrend_func_constant(co2_monthly):
     data = co2_monthly
     periods = Autoperiod.detect(data, detrend_func="constant")
-    assert len(periods) == 0
+    assert 12 not in periods
 
 
 def test_co2_weekly_autoperiod_detrend_func_constant_window_func_blackman(co2_weekly):
