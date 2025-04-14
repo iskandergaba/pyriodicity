5d9adb65a4e7d5c3c88d7231b4b31b9c351c4e79
Iskander Gaba <iskander@hey.com>
2025-04-14 14:56:53 +0200

    Simplify conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,3 @@
-from typing import Generator
-
 import numpy as np
 import pytest
 from scipy.signal import sawtooth
@@ -22,32 +20,32 @@ def sinewave_100():
 
 
 @pytest.fixture
-def sinewave_10_stream() -> Generator[float, None, None]:
+def sinewave_10_stream():
     return (sample for sample in sinewave(1000, 10, 1))
 
 
 @pytest.fixture
-def sinewave_50_stream() -> Generator[float, None, None]:
+def sinewave_50_stream():
     return (sample for sample in sinewave(1000, 50, 1))
 
 
 @pytest.fixture
-def sinewave_100_stream() -> Generator[float, None, None]:
+def sinewave_100_stream():
     return (sample for sample in sinewave(1000, 100, 1))
 
 
 @pytest.fixture
-def sinewave_10_batch_size_100_generator() -> Generator[float, None, None]:
+def sinewave_10_batch_size_100_generator():
     return data_generator(sinewave(1000, 10, 1), batch_size=100)
 
 
 @pytest.fixture
-def sinewave_50_batch_size_100_generator() -> Generator[float, None, None]:
+def sinewave_50_batch_size_100_generator():
     return data_generator(sinewave(1000, 50, 1), batch_size=100)
 
 
 @pytest.fixture
-def sinewave_100_batch_size_100_generator() -> Generator[float, None, None]:
+def sinewave_100_batch_size_100_generator():
     return data_generator(sinewave(1000, 100, 1), batch_size=100)
 
 
@@ -67,32 +65,32 @@ def trianglewave_100():
 
 
 @pytest.fixture
-def trianglewave_10_stream() -> Generator[float, None, None]:
+def trianglewave_10_stream():
     return (sample for sample in trianglewave(1000, 10, 1))
 
 
 @pytest.fixture
-def trianglewave_50_stream() -> Generator[float, None, None]:
+def trianglewave_50_stream():
     return (sample for sample in trianglewave(1000, 50, 1))
 
 
 @pytest.fixture
-def trianglewave_100_stream() -> Generator[float, None, None]:
+def trianglewave_100_stream():
     return (sample for sample in trianglewave(1000, 100, 1))
 
 
 @pytest.fixture
-def trianglewave_10_batch_size_100_generator() -> Generator[float, None, None]:
+def trianglewave_10_batch_size_100_generator():
     return data_generator(trianglewave(1000, 10, 1), batch_size=100)
 
 
 @pytest.fixture
-def trianglewave_50_batch_size_100_generator() -> Generator[float, None, None]:
+def trianglewave_50_batch_size_100_generator():
     return data_generator(trianglewave(1000, 50, 1), batch_size=100)
 
 
 @pytest.fixture
-def trianglewave_100_batch_size_100_generator() -> Generator[float, None, None]:
+def trianglewave_100_batch_size_100_generator():
     return data_generator(trianglewave(1000, 100, 1), batch_size=100)
 
 
